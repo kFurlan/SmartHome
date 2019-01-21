ca3eb85d4d46d5119c58003fc1af4b2eb5f65cad
fuzzyqu <fuzzyqu@protonmail.com>
2019-01-21 17:28:44 -0300

    Dropdown adicionado na criação de cômodos

--- app/lib/pages/room_create.dart
+++ app/lib/pages/room_create.dart
@@ -6,6 +6,7 @@ class RoomCreatePage extends StatefulWidget {
 }
 
 class _RoomCreatePageState extends State<RoomCreatePage> {
+  String dropDownValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,8 +36,48 @@ class _RoomCreatePageState extends State<RoomCreatePage> {
         child: Column(
           children: <Widget>[
             TextFormField(
-              decoration: InputDecoration(labelText: 'Cômodo'),
+              decoration: InputDecoration(labelText: 'Nome do Cômodo'),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: dropDownValue,
+                hint: Text('Tipo de cômodo'),
+                onChanged: (String value) {
+                  setState(() {
+                    this.dropDownValue = value;
+                  });
+                  debugPrint(value);
+                },
+                items: <DropdownMenuItem<String>>[
+                  DropdownMenuItem(
+                    value: 'Quarto',
+                    child: Text('Quarto'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Banheiro',
+                    child: Text('Banheiro'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Garagem',
+                    child: Text('Garagem'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Sala de estar',
+                    child: Text('Sala de estar'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Cozinha',
+                    child: Text('Cozinha'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Escritório',
+                    child: Text('Escritório'),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
