cb68508b2af010720ee7c9fdb90a3939c082a9c6
ackk <fuzzyqu@protonmail.com>
2019-01-25 23:16:40 -0300

    Ajustes na UI

--- app/lib/pages/profile_edit.dart
+++ app/lib/pages/profile_edit.dart
@@ -10,74 +10,72 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Editar Perfil'),
-          leading: IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {
-              if (Navigator.canPop(context)) {
-                Navigator.pop(context);
-              }
-            },
-          ),
-          elevation: 0.0,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.check),
-              onPressed: () {},
-            )
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Editar Perfil'),
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+            if (Navigator.canPop(context)) {
+              Navigator.pop(context);
+            }
+          },
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 24.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Stack(
-                  alignment: Alignment.bottomRight,
-                  children: <Widget>[
-                    CircleAvatar(
-                      radius: 64.0,
-                      backgroundImage: AssetImage('images/human.png'),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Stack(
+                alignment: Alignment.bottomRight,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 64.0,
+                    backgroundImage: AssetImage('images/human.png'),
+                  ),
+                  Positioned(
+                    top: 80,
+                    left: 80,
+                    child: IconButton(
+                      icon: Icon(Icons.add_circle),
+                      iconSize: 40.0,
+                      onPressed: () {},
                     ),
-                    Positioned(
-                      top: 80,
-                      left: 80,
-                      child: IconButton(
-                        icon: Icon(Icons.add_circle),
-                        iconSize: 40.0,
-                        onPressed: () {},
-                      ),
-                    )
-                  ],
+                  )
+                ],
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'NOME',
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'NOME',
-                  ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'DATA DE NASCIMENTO',
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'DATA DE NASCIMENTO',
-                  ),
-                  keyboardType: TextInputType.datetime,
+                keyboardType: TextInputType.datetime,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'NÚMERO DE TELEFONE',
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'NÚMERO DE TELEFONE',
-                  ),
-                  keyboardType: TextInputType.phone,
+                keyboardType: TextInputType.phone,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'SENHA',
                 ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'SENHA',
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- app/lib/pages/profile_info.dart
+++ app/lib/pages/profile_info.dart
@@ -11,45 +11,43 @@ class _ProfileInfoPageState extends State<ProfileInfoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24.0),
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 12.0),
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 32.0,
-                  backgroundImage: AssetImage('images/human.png'),
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24.0),
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 12.0),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 32.0,
+                backgroundImage: AssetImage('images/human.png'),
+              ),
+              trailing: OutlineButton(
+                child: Text(
+                  'EDITAR PERFIL',
                 ),
-                trailing: OutlineButton(
-                  child: Text(
-                    'EDITAR PERFIL',
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ProfileEditPage(),
-                      ),
-                    );
-                  },
-                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProfileEditPage(),
+                    ),
+                  );
+                },
               ),
             ),
-            ListTile(
-              title: Text(
-                'Nome do Usuário',
-                style: TextStyle(fontSize: 24.0),
-              ),
-              subtitle: Text('email@email.com'),
+          ),
+          ListTile(
+            title: Text(
+              'Nome do Usuário',
+              style: TextStyle(fontSize: 24.0),
             ),
-            Divider()
-          ],
-        ),
-      )),
-    );
+            subtitle: Text('email@email.com'),
+          ),
+          Divider()
+        ],
+      ),
+    ));
   }
 }
