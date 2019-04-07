9fa6e0121238f5c521a23c5a1d458f2c9aad810c
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-04-06 21:09:59 -0400

    Adicioando tela de edição de dispositivos

---
+++ smartapp/lib/ui/device/device_edit_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DeviceEditPage extends StatefulWidget {
+  final Device model;
+
+  const DeviceEditPage({@required this.model});
+
+  @override
+  DeviceEditPageState createState() => DeviceEditPageState();
+}
+
+class DeviceEditPageState extends State<DeviceEditPage> {
+  DeviceBloc _deviceBloc;
+  String _deviceType;
+  String _deviceName;
+  final DeviceCreateKey = GlobalKey<FormState>()
+
+  @override
+  void initState() {
+    this._deviceBloc = BlocProvider.of<DeviceBloc>(context);
+    this._deviceName = widget.model.name;
+    this._deviceType = widget.model.type;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Editar dispositivo'),
+        leading: IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              if (Navigator.canPop(context)) {
+                Navigator.pop(context);
+              }
+            }
+        ),
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                initialValue: _deviceName,
+                maxLength: 60,
+                validator: (String value) {
+                  if (value == null || value.isEmpty) {
+                    return "Por favor! Digite o nome do dispositivo";
+                  }
+                },
+                onSaved: (String value) {
+                  this._deviceName = value;
+                },
+                decoration: InputDecoration(
+                  labelText: 'Nome do dispositivo'
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: RaisedButton(
+                  padding: EdgeInsets.all(16),
+                  child: Text('Salvar'),
+                  onPressed: () {
+                    if (_deviceCreateKey.currentState.validate()) {
+                      _deviceCreateKey.currentState.save();
+                      final _model = Device.withId(
+                          id: widget.model.id,
+                          name: _deviceName,
+                          type: _deviceType);
+                      _deviceBloc.dispatch(
+                        EditDevice(model: _model),
+                      );
+                      if (Navigator.canPop(context)) {
+                        Navigator.pop(context);
+                      }
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: RaisedButton(
+                    padding: EdgeInsets.all(16),
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    child: Text('EXCLUIR'),
+                    onPressed: () {
+                      _deviceBloc.dispatch(
+                        DeleteDevice(id: widget.model.id)
+                      );
+                      if(Navigator.canPop(context)) {
+                        Navigator.pop(context)
+                      }
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
